77ad9bd564dee8db7b2a64133cf03bf1750696f7
SealsRock12 <git@mail.toad39.dev>
2021-06-08 21:25:35 +0000

    pywindow functionality

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 /* these functions are made by me (forester2015), not the offical AoPS code */
 
+/**
+ * Developer info:
+ *  - Font/CORS errors: It seems like AoPs doesn't have the Roboto-Regular.ttf
+ *    file anymore, but it is still in their CSS. This makes an error on the
+ *    real website too. This shouldn't be much of a problem.
+ */
+
 function show_hide(id) {
   if (document.querySelector(`#${id}`).style.display == "none") {
     document.querySelector(`#${id}`).style.display = "block";
